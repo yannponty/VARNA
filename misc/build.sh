427814cdd89961efaf3ff4c2241dc7b837716886
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2023-11-17 12:56:56 +0100

    updated varna scripts

--- misc/build.sh
+++ misc/build.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-ant compile
-ant jar
+ant -f nbbuild.xml jar
 
 mkdir -p ${PREFIX}/lib/varna
-cp build/jar/VARNA*.jar ${PREFIX}/lib/varna/
+cp dist/VARNA*.jar ${PREFIX}/lib/varna/
 cp misc/varna.sh ${PREFIX}/bin/varna
--- misc/varna.sh
+++ misc/varna.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-java -jar ${CONDA_PREFIX}/lib/varna/VARNAcmd.jar $@
+java -jar ${CONDA_PREFIX}/lib/varna/VARNA.jar $@
